84f3bfcb31a3f99f26911f0ff138a8bd8c843ce5
Chuck Fan <fanck0605@qq.com>
2021-12-22 22:28:48 +0800

    Move test files to ./tests

--- jest.config.js
+++ jest.config.js
@@ -30,7 +30,7 @@ module.exports = {
   coveragePathIgnorePatterns: [
     "<rootDir>/lib/",
     "<rootDir>/node_modules/",
-    "<rootDir>/test/",
+    "<rootDir>/tests/",
   ],
 
   // A list of reporter names that Jest uses when writing coverage reports
